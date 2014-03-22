861408b346b74cb70626ab7e3a06f490018fcb3c
Dominik Schwind <dschwind@lostfocus.de>
2014-03-22 22:52:48 +0100

    We need this because we sideload and want to get the ids.

--- helpers.php
+++ helpers.php
@@ -4,3 +4,34 @@ function instagramy_goodness_redirecturl(){
     $redirecturl = (isset($_SERVER["HTTPS"]) ? "https://" : "http://") . $_SERVER['HTTP_HOST'] . "/wp-admin/tools.php?page=instagramy_goodness";
     return $redirecturl;
 }
+
+/*
+ * Yes, this is basically the media_sideload_image function.
+ * But I need the id, not the HTML code.
+ */
+function instagramy_goodness_sideload_image($file, $post_id, $desc = null){
+    if ( ! empty($file) ) {
+        // Download file to temp location
+        $tmp = download_url( $file );
+
+        // Set variables for storage
+        // fix file filename for query strings
+        preg_match( '/[^\?]+\.(jpe?g|jpe|gif|png)\b/i', $file, $matches );
+        $file_array['name'] = basename($matches[0]);
+        $file_array['tmp_name'] = $tmp;
+
+        // If error storing temporarily, unlink
+        if ( is_wp_error( $tmp ) ) {
+            @unlink($file_array['tmp_name']);
+            $file_array['tmp_name'] = '';
+        }
+
+        // do the validation and storage stuff
+        $id = media_handle_sideload( $file_array, $post_id, $desc );
+        // If error storing permanently, unlink
+        if ( is_wp_error($id) ) {
+            @unlink($file_array['tmp_name']);
+        }
+        return $id;
+    }
+}
